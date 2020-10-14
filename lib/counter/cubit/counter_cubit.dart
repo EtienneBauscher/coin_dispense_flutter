42a9e7152be92f65ebc94d722ca6971bcd58a7a9
Etienne Bauscher <44187788+EtienneBauscher@users.noreply.github.com>
2020-10-14 20:08:34 +0200

    Update counter_cubit.dart

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -1,15 +1,13 @@
 import 'package:bloc/bloc.dart';
 
-/// {@template counter_cubit}
-/// A [Cubit] which manages an [int] as its state.
-/// {@endtemplate}
+
 class CounterCubit extends Cubit<double> {
-  /// {@macro counter_cubit}
+ 
   CounterCubit() : super(450.50);
 
-  /// Add 1 to the current state.
+ 
   void resetcounter() => emit(450.50);
 
-  /// Subtract 1 from the current state.
+ 
   void decrement(double submittedAmount) => emit(submittedAmount - state);
 }
