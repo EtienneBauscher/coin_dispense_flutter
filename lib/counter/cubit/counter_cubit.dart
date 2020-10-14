e2f66e98776d87dc9a01bb47a9dbaebc09da6a03
Etienne Bauscher <44187788+EtienneBauscher@users.noreply.github.com>
2020-10-14 20:09:05 +0200

    Update counter_cubit.dart

--- lib/counter/cubit/counter_cubit.dart
+++ lib/counter/cubit/counter_cubit.dart
@@ -1,13 +1,7 @@
 import 'package:bloc/bloc.dart';
 
-
 class CounterCubit extends Cubit<double> {
- 
   CounterCubit() : super(450.50);
-
- 
   void resetcounter() => emit(450.50);
-
- 
   void decrement(double submittedAmount) => emit(submittedAmount - state);
 }
