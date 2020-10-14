7c8347bc0196879f7b3cf2bf1fa89e51ca8b4881
Etienne Bauscher <44187788+EtienneBauscher@users.noreply.github.com>
2020-10-14 16:28:43 +0200

    Create money.dart

---
+++ lib/counter/models/money.dart
@@ -0,0 +1,13 @@
+class Money {
+  static const balance = 0.0;
+  static const double twohundred = 200.00;
+  static const double onehundred = 100.00;
+  static const double fifty = 50.00;
+  static const double twenty = 20.00;
+  static const double ten = 10.00;
+  static const double five = 5.00;
+  static const double two = 2.00;
+  static const double one = 1.00;
+  static const double fiftycents = 0.50;
+  static const double twentycents = 0.20;
+}
