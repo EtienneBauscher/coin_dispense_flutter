5358a93912abbd75ea9b8e483631ce3e9ad4c07e
Etienne Bauscher <44187788+EtienneBauscher@users.noreply.github.com>
2020-10-14 20:09:54 +0200

    Update counter_observer.dart

--- lib/counter_observer.dart
+++ lib/counter_observer.dart
@@ -1,9 +1,5 @@
 import 'package:bloc/bloc.dart';
 
-/// {@template counter_observer}
-/// [BlocObserver] for the counter application which
-/// observes all [Cubit] state changes.
-/// {@endtemplate}
 class CounterObserver extends BlocObserver {
   @override
   void onChange(Cubit cubit, Change change) {
